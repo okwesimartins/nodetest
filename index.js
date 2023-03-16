7d63af15eb02f8d4de97b163091b0d0f7ba0b44c
macbook <macbook@macbooks-MacBook-Pro.local>
2023-03-16 11:04:01 +0100

    new

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import router from "./routes/user-routes.js";
 import blogRouter from "./routes/blog-routes.js";
 import bodyParser from "body-parser";
 
-import multer from 'multer';
+
 dotenv.config();
 const app = express();
 
@@ -19,7 +19,7 @@ app.use("/api/blog", blogRouter);
 
 mongoose.set('strictQuery', false);
 
-    mongoose.connect(`mongodb+srv://martin:${ process.env.DB_PASSWORD}@mycluster.b4a3mnh.mongodb.net/?retryWrites=true&w=majority`
+    mongoose.connect(process.env.MONGO_URI
 ).then(() => app.listen(5000)).then(() => console.log("connected")).catch((err) => console.log(err));
 
 
